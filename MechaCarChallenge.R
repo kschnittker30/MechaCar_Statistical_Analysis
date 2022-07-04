1d84d8b4ec960f23d58381b691d9ece583c027a3
Klaudia Schnittker <kschnittker30@gmail.com>
2022-07-04 13:13:38 -0700

    Deliverable 2 complete.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,5 @@
 install.packages("tidyverse")
+library(dplyr)
 
 # Read csv file.
 mecha_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -8,4 +9,16 @@ mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cl
  
 #Summarize linear model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_table)) 
+
+# Read csv file.
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Summarize() function to get the mean, median, variance, and standard deviation
+total_summary <- coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI) ,SD=sd(PSI))
+
+# summarize() functions to group each manufacturing lot
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI) ,SD=sd(PSI), .groups = 'keep')
+
+
+
 
